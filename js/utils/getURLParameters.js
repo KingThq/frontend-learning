41eda1afdb15be6c561fb03ccd74ab6d0a2604bc
KingThq <1581563341@qq.com>
2021-12-06 19:00:16 +0800

    fix(js/utils): fix getURLParameters match result

--- js/utils/getURLParameters.js
+++ js/utils/getURLParameters.js
@@ -5,14 +5,12 @@
 
 const url = 'http://test.com?page=1&size=10';
 
-const getURLParameters = url => (
-  url
-    .match(/([^?=&]+)(=([^&]*))/g || [])
+const getURLParameters = url => (url
+    .match(/([^?=&]+)(=([^&]*))/g) || [])
     .reduce((a, v) => {
     a[v.slice(0, v.indexOf('='))] = v.slice(v.indexOf('=') + 1);
     return a;
-  }, {})
-);
+  }, {});
 
 const result = getURLParameters(url);
 console.log(result)
