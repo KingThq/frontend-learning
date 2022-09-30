1159a88842f935ef2f93b51b11d26a3e15c11d13
KingThq <1581563341@qq.com>
2022-09-30 15:42:30 +0800

    feat(vue/renderer): add text and comment renderer

---
+++ vue/renderer/mountAndUpdate/TextAndComment.js
@@ -0,0 +1,256 @@
+/**
+ * @title 文本节点和注释节点
+ * 
+ * <div><!-- 注释节点 -->我是文本节点</div>
+ */
+
+// 文本节点的 type 标识
+const Text = Symbol();
+const textVnode = {
+  type: Text,
+  children: '我是文本内容',
+};
+
+// 注释节点的 type 标识
+const Comment = Symbol();
+const commentVnode = {
+  type: Comment,
+  children: '我是注释内容',
+};
+
+ function createRenderer(options) {
+  const {
+    createElement,
+    setElementText,
+    insert,
+    createText,
+    setText,
+    createComment,
+    patchProps,
+  } = options;
+
+  function mountElement(vnode, container) {
+    const el = vnode.el = createElement(vnode.type);
+    if (typeof vnode.children === 'string') {
+      setElementText(el, vnode.children);
+    } else if (Array.isArray(vnode.children)) {
+      vnode.children.forEach(child => {
+        patch(null, child, el);
+      });
+    }
+
+    if (vnode.props) {
+      for (const key in vnode.props) {
+        patchProps(el, key, null, vnode.props[key]);
+      }
+    }
+
+    insert(el, container);
+  }
+
+  function patchElement(n1, n2) {
+    const el = n2.el = n1.el;
+    const oldProps = n1.props;
+    const newProps = n2.props;
+    // 第一步：更新 props
+    for (const key in newProps) {
+      if (newProps[key] !== oldProps[key]) {
+        patchProps(el, key, oldProps[key], newProps[key]);
+      }
+    }
+    for (const key in oldProps) {
+      if (!(key in newProps)) {
+        patchProps(el, key, oldProps[key], null);
+      }
+    }
+
+    // 第二步：更新 children
+    patchChildren(n1, n2, el);
+  }
+
+  function patchChildren(n1, n2, container) {
+    if (typeof n2.children === 'string') {
+      if (Array.isArray(n1.children)) {
+        n1.children.forEach(c => unmount(c));
+      }
+      setElementText(container, n2.children);
+    } else if (Array.isArray(n2.children)) {
+      if (Array.isArray(n1.children)) {
+        n1.children.forEach(c => unmount(c));
+        n2.children.forEach(c => patch(null, c, container));
+      } else {
+        setElementText(container, '');
+        n2.children.forEach(c => patch(null, c, container));
+      }
+    } else {
+      if (Array.isArray(n1.children)) {
+        n1.children.forEach(c => unmount(c));
+      } else if (typeof n1.children === 'string') {
+        setElementText(container, '');
+      }
+      // 如果也没有旧子节点，则什么都不需要做
+    }
+  }
+
+  function patch(n1, n2, container) {
+    if (n1 && n1.type !== n2.type) {
+      unmount(n1);
+      n1 = null;
+    }
+    const { type } = n2;
+    if (typeof type === 'string') {
+      if (!n1) {
+        mountElement(n2, container);
+      } else {
+        patchElement(n1, n2);
+      }
+    } else if (type === Text) {
+      // 如果新 vonde 的类型是 Text，则说明该 vnode 描述的是文本节点
+      if (!n1) {
+        // 使用 createTextNode 创建文本节点
+        const el = n2.el = createText(n2.children);
+        insert(el, container);
+      } else {
+        // 如果旧 vnode 存在，只需要使用新文本节点的文本内容更新就文本节点即可
+        const el = n2.el = n1.el;
+        if (n2.children !== n1.children) {
+          setText(el, n2.children);
+        }
+      }
+    } else if (type === Comment) {
+      if (!n1) {
+        const el = n2.el = createComment(n2.children);
+        insert(el, container);
+      } else {
+        const el = n2.el = n1.el;
+        if (n2.children !== n1.children) {
+          setText(el, n2.children);
+        }
+      }
+    }
+  }
+
+  function render(vnode, container) {
+    if (vnode) {
+      patch(container._vnode, vnode, container);
+    } else {
+      if (container._vnode) {
+        unmount(container._vnode);
+      }
+    }
+    container._vnode = vnode;
+  }
+
+  return { render };
+}
+
+function unmount(vnode) {
+  const parent = vnode.el.parentNode;
+  if (parent) {
+    parent.removeChild(vnode.el);
+  }
+}
+
+function shouldSetAsProps(el, key, value) {
+  if (key === 'form' && el.tagName === 'INPUT') return false;
+  return key in el;
+}
+
+const renderer = createRenderer({
+  createElement(tag) {
+    return document.createElement(tag);
+  },
+  setElementText(el, text) {
+    el.textContent = text;
+  },
+  insert(el, parent, anchor = null) {
+    parent.insertBefore(el, anchor);
+  },
+  createText(text) {
+    return document.createTextNode(text);
+  },
+  setText(el, text) {
+    el.nodeValue = text;
+  },
+  createComment(comment) {
+    return document.createComment(comment);
+  },
+  patchProps(el, key, prevValue, nextValue) {
+    if (/^on/.test(key)) {
+      const invokers = el._vei || (el._vei = {});
+      let invoker = invokers[key];
+      const name = key.slice(2).toLocaleLowerCase();
+      if (nextValue) {
+        if (!invoker) {
+          invoker = el._vei[key] = (e) => {
+            if (e.timeStamp < invoker.attached) return;
+            if (Array.isArray(invoker.value)) {
+              invoker.value.forEach(fn => fn(e));
+            } else {
+              invoker.value(e);
+            }
+          }
+          invoker.value = nextValue;
+          invoker.attached = performance.now();
+          el.addEventListener(name, invoker);
+        } else {
+          invoker.value = nextValue;
+        }
+      } else if (invoker) {
+        el.removeEventListener(name, invoker);
+      }
+    } else if (key === 'class') {
+      el.className = nextValue || '';
+    } else if (shouldSetAsProps(el, key, nextValue)) {
+      const type = typeof el[key];
+      if (typeof type === 'boolean' && nextValue === '') {
+        el[key] = true;
+      } else {
+        el[key] = nextValue;
+      }
+    } else {
+      el.setAttribute(key, nextValue);
+    }
+  }
+});
+
+
+const { effect, ref } = VueReactivity;
+const bol = ref(false);
+
+effect(() => {
+  // const vnode = {
+  //   type: 'div',
+  //   props: {
+  //     onClick: () => {
+  //       bol.value = true;
+  //     }
+  //   },
+  //   children: !bol.value ? 'click me' : null,
+  // }
+
+  const vnode = {
+    type: 'div',
+    children: [
+      {
+        type: 'p',
+        props: {
+          onClick: () => {
+            bol.value = true;
+          }
+        },
+        children: 'click me',
+      },
+      {
+        type: Text,
+        children: !bol.value ? 'I am Text' : 'Text changed',
+      },
+      {
+        type: Comment,
+        children: !bol.value ? 'I am Comment' : 'Comment changed',
+      },
+    ],
+  }
+
+  renderer.render(vnode, document.getElementById('app'));
+});
