eea7ca977a8b31a331d5af3fdec7261b7fe1a320
KingThq <1581563341@qq.com>
2022-12-19 10:49:58 +0800

    feat(big-react): first exploration reconciler

---
+++ react/big-react/packages/react-reconciler/src/workLoop.ts
@@ -0,0 +1,58 @@
+import { FiberNode } from './fiber';
+import { beginWork } from './beginWork';
+import { completeWork } from './completeWork';
+
+// 指向当前正在工作的 fiberNode
+let workInProgress: FiberNode | null = null;
+
+function prepareFreshStack(fiber: FiberNode) {
+	workInProgress = fiber;
+}
+
+function renderRoot(root: FiberNode) {
+	// 初始化
+	prepareFreshStack(root);
+
+	do {
+		try {
+			workLoop();
+			break;
+		} catch (error) {
+			console.warn('workLoop 发生错误', error);
+			workInProgress = null;
+		}
+	} while (true);
+}
+
+function workLoop() {
+	while (workInProgress !== null) {
+		performUnitOfWork(workInProgress);
+	}
+}
+
+function performUnitOfWork(fiber: FiberNode) {
+	const next = beginWork(fiber);
+	fiber.memorizedProps = fiber.pendingProps;
+
+	if (next === null) {
+		completeUnifOfWork(fiber);
+	} else {
+		workInProgress = next;
+	}
+}
+
+function completeUnifOfWork(fiber: FiberNode) {
+	let node: FiberNode | null = fiber;
+
+	do {
+		completeWork(node);
+		const sibling = node.sibling;
+
+		if (sibling !== null) {
+			workInProgress = sibling;
+			return;
+		}
+		node = node.return;
+		workInProgress = node;
+	} while (node !== null);
+}
