591709d7f4c4fd3e74a42251e769995e21ae2d7f
KingThq <1581563341@qq.com>
2023-01-03 17:19:22 +0800

    feat(deepclone): update deepClone

--- js/deepClone.js
+++ js/deepClone.js
@@ -6,23 +6,29 @@
 // 循环引用：obj.obj = obj;
 
 const cloneDeep = (target = {}, map = new Map()) => {
-  if (typeof target !== 'object') {
+  if (typeof target !== "object") {
     return target;
   }
-
-  const isArray = Array.isArray(target);
-  // 保存结果
-  let cloneTarget = isArray ? [] : {};
-
   // 检查map中有无克隆过的对象 有，直接返回, 没有,将当前对象作为key，克隆对象作为value进行存储
   if (map.get(target)) {
     return map.get(target);
   }
+
+  // 加 || 的原因是为了防止 Array 的 prototype 被重写
+  const isArray =
+    Array.isArray(target) ||
+    Object.prototype.toString.call(target) === "[object Array]";
+  // 保存结果
+  let cloneTarget = isArray ? [] : {};
+
   // 防止循环引用
   map.set(target, cloneTarget);
 
   for (const key in target) {
-    cloneTarget[key] = cloneDeep(target[key], map);
+    if (target.hasOwnProperty(key)) {
+      // 保证 key 不是原型属性
+      cloneTarget[key] = cloneDeep(target[key], map);
+    }
   }
 
   return cloneTarget;
