024534e97033580c261a6fe26402cd2d07a3851e
KingThq <1581563341@qq.com>
2023-02-07 12:11:22 +0800

    feat(js/call): update

--- js/call.js
+++ js/call.js
@@ -2,21 +2,24 @@
  * @title call 实现
  */
 
-Function.prototype.myCall = function(context) {
-  if (typeof this !== 'function') {
-    throw new Error('Type Error');
+Function.prototype.myCall = function (context) {
+  if (typeof this !== "function") {
+    throw new Error("Type Error");
   }
+
+  console.log("this:", this);
+  console.log("context:", context);
 
   // 判断 context 是否传入，如果没有传就设置为 window
   context = context || window;
   // this 即为我们要调用的方法
   context.fn = this;
 
-  console.log('arguments:', arguments)
+  console.log("arguments:", arguments);
 
   const args = [...arguments].slice(1);
 
-  console.log('args:', args)
+  console.log("args:", args);
 
   let result = null;
   result = context.fn(...args);
@@ -30,10 +33,10 @@ const obj = {
 const a = 2;
 
 function func(s1, s2) {
-  console.log('s1:', s1);
-  console.log('s2:', s2);
+  console.log("s1:", s1);
+  console.log("s2:", s2);
   console.log(this.a);
 }
 
 func.myCall(obj, 9, 8); // 1
-func.myCall(null); // 2
+// func.myCall(null); // 2
