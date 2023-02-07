93b118aa4fa241feb5a1844b86412d97bb8c1557
KingThq <1581563341@qq.com>
2023-02-07 12:25:12 +0800

    feat(js/apply): update

--- js/apply.js
+++ js/apply.js
@@ -2,23 +2,26 @@
  * @title apply 实现
  */
 
-Function.prototype.myApply = function(context) {
-  if (typeof this !== 'function') {
-    throw new Error('Type Error');
+Function.prototype.myApply = function (context) {
+  if (typeof this !== "function") {
+    throw new Error("Type Error");
   }
 
   context = context || window;
-  context.fn = this;
-  
-  console.log('arguments[1]:', arguments[1])
+  // 使用 Symbol 来保证属性唯一
+  // 也就是保证不会重写用户自己原来定义在 context 中的同名属性
+  const fnSymbol = Symbol();
+  context[fnSymbol] = this;
+
+  console.log("arguments[1]:", arguments[1]);
 
   let result = null;
   if (arguments[1]) {
-    result = context.fn(...arguments[1]);
+    result = context[fnSymbol](...arguments[1]);
   } else {
-    result = context.fn();
+    result = context[fnSymbol]();
   }
-  delete context.fn;
+  delete context[fnSymbol];
   return result;
 };
 
@@ -26,7 +29,7 @@ const obj = {
   a: 1,
 };
 function func() {
-  console.log('func arguments:', arguments);
+  console.log("func arguments:", arguments);
   console.log(this.a);
 }
 func.myApply(obj, [9, 8]);
